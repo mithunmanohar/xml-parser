06289466518b762f535aa0e928b1e22ffc332971
mithunmanohar <mithunmanohar79@gmail.com>
2018-05-01 17:47:16 +0530

    unzip file logic

--- src/main/start_process.py
+++ src/main/start_process.py
@@ -19,12 +19,12 @@ from src.utilities import utils
 
 # Global Constants
 
-ZIP_FILE_LOCATION = ""
+ZIP_FILE_FOLDER = r'F:\usefuk\fiverr\may2018\jaboo\xml-parser\input_zip'
 
 
 
 
-def check_and_process_zip(zip_file_location):
+def check_and_process_zip(zip_file_folder):
     """
     Checks if given zip file path exists, creates a temp folder in the
     location, unzips the files to the temp folder and returns the temp
@@ -37,12 +37,20 @@ def check_and_process_zip(zip_file_location):
 
     """
 
-    if os.path.isdir(zip_file_location):
-        print dir(zip)
+    if os.path.isdir(zip_file_folder):
+        logging.info('ZIP file folder exists')
     else:
-        logging.error('Zip file location not found')
-    #file_list, file_location = utils.unzip()
+        logging.error('Zip file folder do not exist')
+
+    unzip_file_folder = zip_file_folder + os.sep + 'temp'
+
+    if not os.path.exists(unzip_file_folder):
+        logging.info('Created unzip folder %s ' %s unzip_file_folder )
+        os.mkdir(unzip_file_folder)
+
+    file_list, file_location = utils.unzip_files(zip_file_folder,
+                                                 unzip_file_folder)
 
 if __name__ == '__main__':
     utils.setup_logger(logging.INFO, '../../logs/start_process.log')
-    file_folder = check_and_process_zip(ZIP_FILE_LOCATION)
+    file_folder = check_and_process_zip(ZIP_FILE_FOLDER)
--- src/utilities/utils.py
+++ src/utilities/utils.py
@@ -7,6 +7,7 @@ created on : 1-MAY-2018
 # Global imports
 import os
 import sys
+import glob
 import zipfile
 import logging
 
@@ -28,7 +29,7 @@ def setup_logger(log_level, log_file_path):
 
     logging.basicConfig(
         filename=log_file_path,
-        format='%(asctime)s %(levelname)s: %(message)s',
+        format='[%(asctime)s] [%(levelname)s]: %(message)s',
         datefmt='%m/%d/%Y %I:%M:%S %p',
         level=log_level
         )
@@ -38,7 +39,7 @@ def setup_logger(log_level, log_file_path):
 def read_excel(file_path, mode):
     pass
 
-def unzip_files(zip_file_path):
+def unzip_files(zip_file_folder, unzip_file_folder):
     """
     Validates zip file, unzips it to a temp folder and returns list of files,
     folder path of unzipped files
@@ -49,7 +50,20 @@ def unzip_files(zip_file_path):
         folder path : location of unzipped files
         file list : list of files unzipped
     """
+    zip_file_list = glob.glob(zip_file_folder + os.sep + '*.zip')
+    for zip_file in zip_file_list:
+        if zipfile.is_zipfile(zip_file):
+            logging.info("Processing ZIP file %s" % zip_file)
+            extract_folder = unzip_file_folder + os.sep +\
+                             os.path.basename(zip_file).split('.')[0]
+            print extract_folder
+            if not os.path.exists(extract_folder):
+                os.mkdir(extract_folder)
+            with zipfile.ZipFile(zip_file, 'r') as zip_f:
+                zip_f.extractall(extract_folder)
+                print glob.gloB(extract_folder)
+        else:
+            logging.info("Invalid ZIP file %s" % zip_file)
 
-    print dir(zipfile)
-    if zipfile.is_zipfile(zip_file_path):
-        logger.info("Found a ")
+
+    return '', ''
